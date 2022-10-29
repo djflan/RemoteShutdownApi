8ece6503e601f7450086c134002d7963926db756
Dan Flanigan <djflan@gmail.com>
2022-10-29 15:13:59 -0500

    initial

---
+++ RemoteShutdownApi/Program.cs
@@ -0,0 +1,64 @@
+﻿using RemoteShutdownApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast =  Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateTime.Now.AddDays(index),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
+app.MapPost("/shutdown-{password}", (string password) =>
+{
+    string message = "no";
+
+    if (password == "yes")
+        message = "ok";
+
+    var response = new ShutdownResponse(message);
+
+    if (message == "ok")
+        _ = new Shutdown().Begin();
+
+    return response;
+}).WithName("ShutDown");
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+record ShutdownResponse(string response)
+{
+}
---
+++ RemoteShutdownApi/Shutdown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RemoteShutdownApi
+{
+    public class Shutdown
+    {
+        public Shutdown()
+        {
+        }
+
+        public async Task Begin()
+        {
+            await Task.Run(
+                () =>
+                {
+                    var process = new System.Diagnostics.Process();
+
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "shutdown";
+                    startInfo.Arguments = "/r /f /t 0";
+                    startInfo.UseShellExecute = true;
+                    startInfo.Verb = "runas";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                });
+        }
+    }
+}
+
